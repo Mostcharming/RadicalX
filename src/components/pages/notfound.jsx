16af4ef7bd7b0aab708e05f75711e89569e605d2
Mayowa F Fredrick <mostcharming920@yahoo.com>
2022-11-06 17:20:17 +0100

    Form pages still in progress

--- src/components/pages/notfound.jsx
+++ src/components/pages/notfound.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function notfound() {
   return (
     <div className="">
       <div className="internWrap">
         <div className="internWrap-header">
           <span className="internWrap-text">
-            Page not found, Please go back to Internships
+            Page not found, Please go back to <br />
+            <Link to="/internships">Internships</Link>
           </span>
         </div>
       </div>
